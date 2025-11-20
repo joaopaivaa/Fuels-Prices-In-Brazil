b04e27211572aecc3b9e0fbcc0778eff63ef9fae
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-11-19 22:37:14 -0300

    update silver layer script

--- transformation_silver_layer.py
+++ transformation_silver_layer.py
@@ -1,3 +1,4 @@
+import unicodedata
 import findspark
 findspark.init()
 
@@ -7,9 +8,18 @@ from pyspark.sql.functions import *
 
 spark = (
     SparkSession.builder
-    .appName("Parquet Fix Windows")
+    .appName("Fuel Prices in Brazil")
     .getOrCreate()
 )
+
+def remove_accents(s):
+    if s is None:
+        return None
+    s = unicodedata.normalize('NFD', s)
+    s = "".join(c for c in s if unicodedata.category(c) != "Mn")
+    return s.replace("ç", "c").replace("Ç", "C")
+
+remove_accents_udf = udf(remove_accents, StringType())
 
 df_diesel_cng = spark.read.parquet("bronze/Diesel and CNG Prices.parquet")
 df_lpg = spark.read.parquet("bronze/LPG Prices.parquet")
@@ -45,13 +55,14 @@ df = (
 
 df = (
     df
+    .withColumn('ab_state', col('nm_state'))
     .withColumn('nm_region', when(col('nm_region') == 'N', 'North')
                             .when(col('nm_region') == 'NE', 'Northeast')
                             .when(col('nm_region') == 'CO', 'Central-West')
                             .when(col('nm_region') == 'SE', 'Southeast')
                             .when(col('nm_region') == 'S', 'South')
                             .otherwise(col('nm_region')))
-    .withColumn('nm_region', when(col('nm_state') == 'AC', 'Acre')
+    .withColumn('nm_state', when(col('nm_state') == 'AC', 'Acre')
                             .when(col('nm_state') == 'AL', 'Alagoas')
                             .when(col('nm_state') == 'AP', 'Amapá')
                             .when(col('nm_state') == 'AM', 'Amazonas')
@@ -83,10 +94,28 @@ df = (
 
 df = (
     df
+    .withColumn('nm_fuel_type', when(col('nm_fuel_type') == 'Gnv', 'GNV')
+                               .when(col('nm_fuel_type') == 'Glp', 'GLP')
+                               .otherwise(col('nm_fuel_type')))
+)
+
+df = (
+    df
     .withColumn('nm_unit_of_measurement', regexp_replace(col('nm_unit_of_measurement'), 'mÂ³', 'm³'))
     .withColumn('nm_gas_station', regexp_replace(col('nm_gas_station'), 'ã', 'a'))
     .withColumn('nm_neighborhood', regexp_replace(col('nm_neighborhood'), 'ã', 'a'))
     .withColumn('nm_fuel_brand', regexp_replace(col('nm_fuel_brand'), 'ã', 'a'))
+)
+
+df = (
+    df
+    .withColumn("nm_city_unaccented", remove_accents_udf("nm_city"))
+    .withColumn("nm_state_unaccented", remove_accents_udf("nm_state"))
+)
+
+df = (
+    df
+    .withColumn('key_uf_city_lower', concat(lower(col('ab_state')), lit('_'), regexp_replace(lower(col('nm_city_unaccented')), ' ', '_')))
 )
 
 df.write.mode("overwrite").format("parquet").save("silver/fuels_prices")
