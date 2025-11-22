ea51aa7e0c0076a2aaeda69803df69be15ed4f0e
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-11-22 14:40:30 -0300

    update map script and database

--- shap_file_treatment.py
+++ shap_file_treatment.py
@@ -14,9 +14,10 @@ brazil_cities_shp = gpd.read_file("brazil_cities_shape\\BR_Municipios_2024.shp")
 
 brazil_cities_shp['NM_MUN_NO_ACCENT'] = brazil_cities_shp['NM_MUN'].apply(remove_accents)
 
-brazil_cities_shp['key_uf_city_lower'] = (brazil_cities_shp['SIGLA_UF'] + "_" + brazil_cities_shp['NM_MUN_NO_ACCENT']).str.lower().str.replace(' ', '_')
+brazil_cities_shp['uf_city'] = (brazil_cities_shp['SIGLA_UF'] + "_" + brazil_cities_shp['NM_MUN_NO_ACCENT']).str.lower().str.replace(' ', '_')
 
-brazil_cities_shp = brazil_cities_shp[['key_uf_city_lower', 'geometry']]
+brazil_cities_shp = brazil_cities_shp[['NM_UF', 'NM_MUN_NO_ACCENT', 'uf_city', 'geometry']]
+brazil_cities_shp.columns = ['nm_state', 'nm_city', 'uf_city', 'geometry']
 
 brazil_cities_shp["geometry"] = brazil_cities_shp["geometry"].simplify(tolerance=0.01, preserve_topology=True)
 
@@ -26,8 +27,9 @@ brazil_cities_shp.to_file("brazil_cities_shape_adjusted\\brazil_cities.shp", dri
 
 brazil_states_shp = gpd.read_file("brazil_states_shape\\BR_UF_2024.shp")
 
-brazil_states_shp = brazil_states_shp[['SIGLA_UF', 'geometry']]
-
+brazil_states_shp = brazil_states_shp[['NM_UF', 'geometry']]
+brazil_states_shp.columns = ['nm_state', 'geometry']
+    
 brazil_states_shp["geometry"] = brazil_states_shp["geometry"].simplify(tolerance=0.01, preserve_topology=True)
 
 brazil_states_shp.to_file("brazil_states_shape_adjusted\\brazil_states.shp", driver="ESRI Shapefile")
@@ -36,8 +38,9 @@ brazil_states_shp.to_file("brazil_states_shape_adjusted\\brazil_states.shp", dri
 
 brazil_regions_shp = gpd.read_file("brazil_regions_shape\\BR_Regioes_2024.shp")
 
-brazil_regions_shp = brazil_regions_shp[['SIGLA_RG', 'geometry']]
-
+brazil_regions_shp = brazil_regions_shp[['NM_REGIA', 'geometry']]
+brazil_regions_shp.columns = ['nm_region', 'geometry']
+    
 brazil_regions_shp["geometry"] = brazil_regions_shp["geometry"].simplify(tolerance=0.01, preserve_topology=True)
 
 brazil_regions_shp.to_file("brazil_regions_shape_adjusted\\brazil_regions.shp", driver="ESRI Shapefile")
