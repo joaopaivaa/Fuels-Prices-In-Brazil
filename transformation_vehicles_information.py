694f2be04e70f1327a1d1ab133053b54bd64aad1
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-12-10 00:09:11 -0300

    ajust vehicles information script and database

--- transformation_vehicles_information.py
+++ transformation_vehicles_information.py
@@ -9,6 +9,10 @@ df_vehicles.columns = ['nm_category', 'nm_brand', 'nm_model', 'nm_version', 'cd_
 df_vehicles = df_vehicles[df_vehicles['cd_fuel'] == 'F'].reset_index(drop=True)
 
 df_vehicles['nm_category'] = df_vehicles['nm_category'].str.title()
-df_vehicles['nm_brand'] = df_vehicles['nm_category'].str.title()
+df_vehicles['nm_brand'] = df_vehicles['nm_brand'].str.title()
+df_vehicles['nm_model'] = df_vehicles['nm_model'].str.title()
+df_vehicles['nm_version'] = df_vehicles['nm_version'].str.title()
+
+df_vehicles = df_vehicles.drop(['electric_city_efficiency', 'electric_road_efficiency', 'cd_fuel'], axis=1)
 
 df_vehicles.to_csv('dim_vehicles_efficiency.csv')
