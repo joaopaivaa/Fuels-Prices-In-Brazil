5de8afb5c2ed259c301e7553d7cf70371f6ac609
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-12-13 18:44:25 -0300

    adjusted gold layer transformation to adjust inflation

--- transformation_gold_layer.py
+++ transformation_gold_layer.py
@@ -1,6 +1,10 @@
 import datetime
 import findspark
+import pandas as pd
 findspark.init()
+
+import sys
+print(sys.version)
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
@@ -32,8 +36,10 @@ df = (
 
 most_recent_date = df.select('dt_date_month_start').distinct().orderBy(col('dt_date_month_start').desc()).first()['dt_date_month_start'].strftime('%Y-%m-%d')
 
-list_of_dates = df.select('dt_date_month_start').rdd.flatMap(lambda x: x).collect()
-list_of_values = df.select('avg_fuel_price').rdd.flatMap(lambda x: x).collect()
+# list_of_dates = df.select('dt_date_month_start').rdd.flatMap(lambda x: x).collect()
+# list_of_values = df.select('avg_fuel_price').rdd.flatMap(lambda x: x).collect()
+list_of_dates = [row.dt_date_month_start for row in df.select('dt_date_month_start').collect()]
+list_of_values = [row.avg_fuel_price for row in df.select('avg_fuel_price').collect()]
 
 dates_as_strings = [
     d.isoformat() if isinstance(d, (datetime.date, datetime.datetime)) else d 
@@ -52,5 +58,26 @@ headers = {
 }
 
 values_inflation_adjusted = requests.post('https://financial-utilities-api.onrender.com/inflation_adjustment', json=payload, headers=headers)
+
+if values_inflation_adjusted.status_code != 200:
+    values_inflation_adjusted = requests.post('https://financial-utilities-api.onrender.com/inflation_adjustment', json=payload, headers=headers)
+
+df_inflation_adjusted_values = spark.createDataFrame(pd.DataFrame(values_inflation_adjusted.json()))
+
+df_inflation_adjusted_values = df_inflation_adjusted_values.toDF('date', 'original_value', 'inflation_adjusted_value', 'inflation_perc')
+
+df = (
+    df
+    .join(
+        df_inflation_adjusted_values.select(
+            'date',
+            'original_value',
+            'inflation_adjusted_value'
+        ),
+        (df.dt_date_month_start == df_inflation_adjusted_values.date) &
+        (df.avg_fuel_price == df_inflation_adjusted_values.original_value),
+        how='left'
+    )
+)
 
 df.write.mode("overwrite").format("parquet").save("gold/fuels_prices")
