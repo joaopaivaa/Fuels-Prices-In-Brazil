30a87a59c811687918254963f9562d531b75ccde
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2026-05-01 21:16:02 -0300

    updated vehicles efficiency database columns format
    Former-commit-id: 8900951af5243277582dcabaece8117bd277fea1

--- src/etl_vehicles_information/transformation_vehicles_information.py
+++ src/etl_vehicles_information/transformation_vehicles_information.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-df_vehicles = pd.read_csv('databases/cars_efficiency/inmetro_2025.csv', sep=',', encoding="latin1")
+df_vehicles = pd.read_csv('data/vehicles_efficiency/inmetro_2025.csv', sep=',', encoding='latin1', on_bad_lines='warn')
 
 df_vehicles.columns = ['nm_category', 'nm_brand', 'nm_model', 'nm_version', 'cd_fuel', 'ethanol_city_efficiency',
                        'ethanol_road_efficiency', 'gasoline_city_efficiency', 'gasoline_road_efficiency',
                        'electric_city_efficiency', 'electric_road_efficiency']
 
 df_vehicles = df_vehicles[df_vehicles['cd_fuel'] == 'F'].reset_index(drop=True)
+
+df_vehicles['nm_category'] = df_vehicles['nm_category'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+df_vehicles['nm_brand'] = df_vehicles['nm_brand'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+df_vehicles['nm_model'] = df_vehicles['nm_model'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+df_vehicles['nm_version'] = df_vehicles['nm_version'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
 
 df_vehicles['nm_category'] = df_vehicles['nm_category'].str.title()
 df_vehicles['nm_brand'] = df_vehicles['nm_brand'].str.title()
@@ -15,4 +20,4 @@ df_vehicles['nm_version'] = df_vehicles['nm_version'].str.title()
 
 df_vehicles = df_vehicles.drop(['electric_city_efficiency', 'electric_road_efficiency', 'cd_fuel'], axis=1)
 
-df_vehicles.to_csv('databases/cars_efficiency/dim_vehicles_efficiency.csv')
+df_vehicles.to_csv('data/vehicles_efficiency/dim_vehicles_efficiency.csv', index=False)
